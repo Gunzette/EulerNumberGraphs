e1908e74497a6549921dd4a61739df888829a7ec
Gunzette <totallycisgenderUwU@gmail.com>
2023-12-27 21:12:45 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from manim import *
+from math import floor
+
+config.background_color = ManimColor("#EEEFED")
+
+class Interest(Scene):
+    def construct(self):
+        ax = Axes(
+            x_range=[0, 1.1, 0.1],
+            y_range=[0, 3, 1],
+            axis_config={"include_numbers": True, "stroke_color": BLACK, "fill_color": BLACK},
+        )
+        x = ax.get_x_axis()
+        x.numbers.set_color(BLACK)
+        y = ax.get_y_axis()
+        y.numbers.set_color(BLACK)
+
+        def f1(x):
+            if x < 1:
+                return 1
+            else:
+                return 2
+
+        def f2(x):
+            if x < 0.5:
+                return 1
+            elif x < 1:
+                return (1+(1/2))**1
+            else:
+                return (1+(1/2))**2
+            
+        def f3(x):
+            return (1+(1/4))**(floor(4*x))
+        
+        def f4(x):
+            return (1+(1/1000))**(floor(x*1000))
+        
+
+
+        graph = ax.plot(f4, x_range=[0, 1.0], use_smoothing=False, stroke_color=RED)
+
+        self.add(ax, graph)
